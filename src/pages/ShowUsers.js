57e3c9b54772f94d4d033909b06a0649269b8b66
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-01-30 09:35:19 +0600

    updated components

--- src/pages/ShowUsers.js
+++ src/pages/ShowUsers.js
@@ -4,7 +4,7 @@ import UserView from "../components/UserView"
 function ShowUsers() {
     return (
         <div>
-            <h2 className='font-medium leading-tight text-5xl m-8 text-navy-600 text-center'>All Users</h2>
+            <h2 className='font-medium leading-tight text-5xl m-8 text-navy-600 text-center'>ALL USERS</h2>
             <UserView />
         </div>
     )
